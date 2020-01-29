79572f19f4740e120997bd3d9986bc43dc51e84a
Christian Henry <chenry5724@gmail.com>
2020-01-29 09:50:07 -0600

    final

--- pages/returnValues.js
+++ pages/returnValues.js
@@ -1,15 +1,23 @@
 const pageData= function(){
     
    this.dataPromise= async function(){
-        //promise prevents test from failing before data is present to be tested
-        this.data= new Promise((resolve, reject)=>{
+       
         this.dataA=element(by.xpath('/html/body/footer/ul/li[2]/span[1]/a')).getText()
         this.dataB=element(by.xpath('/html/body/footer/ul/li[2]/span[2]/a')).getText()
         this.dataC=element(by.xpath('/html/body/footer/ul/li[2]/span[3]/a')).getText()
-        })
-        this.results=await this.data
+
+        let data1=element(by.xpath('/html/body/footer/ul/li[2]/span[1]/a')).getText()
+        let data2=element(by.xpath('/html/body/footer/ul/li[2]/span[2]/a')).getText()
+        let data3=element(by.xpath('/html/body/footer/ul/li[2]/span[3]/a')).getText()
+      
         
-        return this.results
+        let res1 = await data1
+        let res2 = await data2 
+        let res3 = await data3
+        console.log('Sales'+" "+res1)
+        console.log('Support'+" "+res2)
+        console.log('Phone'+" "+res3)
+        return [res1,res2,res3]
     }
 
 }
