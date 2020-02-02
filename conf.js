1488fd8f5b4ff7dbff0c7e24f014e737de49050c
ChristianH85 <40803449+ChristianH85@users.noreply.github.com>
2020-02-01 21:35:42 -0600

    Spec fix

--- conf.js
+++ conf.js
@@ -1,6 +1,5 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['todo-spec.js'],
     getPageTimeout: 120000,
     framework: 'jasmine',
     specs: ['spec.js']
